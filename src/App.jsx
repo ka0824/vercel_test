9f0881fe776c8420cf6f34b99e1370d5af119e85
ka0824 <rkgud0824@gmail.com>
2023-04-08 17:06:50 +0900

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return <div className="App">vercel 배포 테스트 중 입니다.</div>;
+}
+
+export default App;
